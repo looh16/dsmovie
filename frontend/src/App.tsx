79e18f430abf9930dc6d83f516afa113d76e37df
looh16 <langacustodio@gmail.com>
2022-01-10 15:47:28 +0200

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,26 @@
+
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.css';
 import Navbar from './components/Navbar';
+import Listing from 'pages/Listing';
+import Form from 'pages/Form';
 import './index.css';
 
 function App() {
   return (
-    <Navbar/>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
