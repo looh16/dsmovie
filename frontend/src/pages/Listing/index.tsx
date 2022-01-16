27fdad8f385d89fd06cf9965a860aeb58400791a
looh16 <langacustodio@gmail.com>
2022-01-16 10:52:58 +0200

    Props

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -3,6 +3,7 @@ import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 import { BASE_URL } from "pages/requests";
 import { useEffect, useState } from "react";
+import { unstable_renderSubtreeIntoContainer } from "react-dom";
 import { MoviePage } from "types/movie";
 
 function Listing() {
@@ -10,18 +11,29 @@ function Listing() {
 
     const [pageNumber, setPageNumber] = useState(0);
 
+    const [page, setPage] = useState<MoviePage>({
+
+        content: [],
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        size: 12,
+        number: 0,
+        first: true,
+        numberOfElements: 0,
+        empty: true
+    });
+
     useEffect(() => {
 
-        axios.get(`${BASE_URL}/movies?size=12&page=0`)
-    .then(response => {
-        const data = response.data as MoviePage;
-        console.log(data);
-        setPageNumber(data.number);
-        
-    });
-     
-    }, []);
-    
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
+            .then(response => {
+                const data = response.data as MoviePage;
+                setPage(data);
+            });
+
+    }, [pageNumber]);
+
 
 
     return (
@@ -32,10 +44,16 @@ function Listing() {
             <div className="container">
 
                 <div className="row">
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3" >
-                        <MovieCard />
 
-                    </div>
+                    {page.content.map(movie => (
+
+                        <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3" >
+                            <MovieCard movie={movie} />
+
+                        </div>
+
+                    ))}
+
 
                 </div>
 
